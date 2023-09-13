6f2c0b81610530a844cdfd0a89bbfb8cbd067e41
Alba Mayol Lozano <alba.mayol@students.salle.url.edu>
2023-09-13 19:58:16 +0200

    Created server and central_server files

---
+++ central_server.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    # server central con una cola 
+    variableCompartida = 0
+    
---
+++ server.py
@@ -0,0 +1,14 @@
+def read():
+
+
+def update(value):
+    
+
+
+if __name__ == "__main__":
+
+    for (i=0; i<10; i++){
+        value = read();
+        update(value+1);
+        sleep(1000);
+    }
