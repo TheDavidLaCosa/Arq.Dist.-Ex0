e30d9047e7e31dec38e57e6d99f236f05be88e25
TheDavidLaCosa <64459822+TheDavidLaCosa@users.noreply.github.com>
2023-09-15 11:57:59 +0200

    The client detecta quan el servidor s'ha desconnectat

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
 import socket
 import threading
+import sys
 
 import utils as u
 
 HEADER_SIZE = 5
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "DISC"
+shutdown = False
 
 
 # Function that adds the header to the message
@@ -23,9 +25,13 @@ def send(s, text):
 def handle_input(sock):
 
     while True:
-        txt = input("Message: ")
-        print(txt)
-        send(sock, txt)
+
+        if shutdown:
+            sock.close()
+        else:
+            txt = input("Message: ")
+            print(txt)
+            send(sock, txt)
 
 if __name__ == "__main__":
 
@@ -40,33 +46,47 @@ if __name__ == "__main__":
     thread = threading.Thread(target=handle_input, args=(s,))
     thread.start()
 
-    # Listening to all the messages
-    while True:
-        full_msg = ""
-        is_new_msg = True
+    try:
 
+        # Listening to all the messages
         while True:
+            full_msg = ""
+            is_new_msg = True
 
-            # Receiving the message in small sizes
-            msg = s.recv(HEADER_SIZE)
+            while True:
 
-            # Reading the length of the message
-            if is_new_msg:
+                # Receiving the message in small sizes
+                msg = s.recv(HEADER_SIZE)
 
-                # Reading the header to know where the length of the message
-                length = int(msg[:HEADER_SIZE])  # The :HEADER_SIZE deletes the spaces after the message
+                # Reading the length of the message
+                if is_new_msg:
 
-                # After reading the message we stop reading the header
-                is_new_msg = False
+                    # Reading the header to know where the length of the message
+                    length = int(msg[:HEADER_SIZE])  # The :HEADER_SIZE deletes the spaces after the message
 
-            # Adding the part of the message received and adding it to the full message
-            full_msg += msg.decode(FORMAT)
+                    # After reading the message we stop reading the header
+                    is_new_msg = False
 
-            # Showing the full message
-            if len(full_msg) - HEADER_SIZE == length:
-                print(full_msg[HEADER_SIZE:])  # The HEADER_SIZE: deletes the size of the message that is being shown
+                # Adding the part of the message received and adding it to the full message
+                full_msg += msg.decode(FORMAT)
 
-                is_new_msg = True
-                full_msg = ""
+                # Showing the full message
+                if len(full_msg) - HEADER_SIZE == length:
+                    print(full_msg[HEADER_SIZE:])  # The HEADER_SIZE: deletes the size of the message that is being shown
+
+                    is_new_msg = True
+                    full_msg = ""
+
+    except ConnectionResetError:
+        print("[ERROR]: The server has disconnected unexpectedly")
+        shutdown = True # TODO: Gestionar que l'usuari escrigui input després de que el servidor s'hagi desconnectat
+        # Closing connection
+        s.close()
+        # TODO: Matar els threads al apagar
+
+        sys.exit()
+
+
+
 
 
